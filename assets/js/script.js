511cf5d9f1e12d8f64a72fb43c561a4ddaf5df0b
unknown <chortythemcflurry@gmail.com>
2022-11-21 19:34:46 -0500

    penny

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 // TODO: Declare any global variables we need
+let total = headsRolls + tailsRolls
+
+let percentHeads = 0
+let percentTails = 0
 
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    
     // This is just a sanity check to make sure your JavaScript script is getting loaded
     // You can remove it once you see it in your browser console in the developer tools
     console.log('Hi')
@@ -16,12 +22,21 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
         // TODO: Use the calculated total to calculate the percentages
+                if (total > 0) {
+            // Calculate percentage of heads and tails
+            percentHeads = Math.round((headsRolls / total) * 100)
+            percentTails = Math.round((tailsRolls / total) * 100)
+        }
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
 
 
     // Clear Button Click Handler
         // TODO: Reset global variables to 0
+         headsRolls = 0
+        tailsRolls = 0
+
+
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
